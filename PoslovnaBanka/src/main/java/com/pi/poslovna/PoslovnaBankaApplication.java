b03e405766b9f5b067e3a34bd77ca5bd1cc55e56
Vuk Novakovic <vuk95novakovic@gmail.com>
2018-05-21 09:00:48 +0200

    Initial commit

---
+++ PoslovnaBanka/src/main/java/com/pi/poslovna/PoslovnaBankaApplication.java
@@ -0,0 +1,12 @@
+package com.pi.poslovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+}
---
+++ PoslovnaBanka/src/test/java/com/pi/poslovna/PoslovnaBankaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pi.poslovna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoslovnaBankaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
