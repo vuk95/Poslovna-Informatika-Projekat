9ef8ac9b793c51de1712cb7fd1c6d3cd980cff45
Dimitrije <dimmihzr@gmail.com>
2018-07-10 16:57:54 +0200

    Zavrsen deo oko eksporta izvoda u xml

--- PoslovnaBanka/src/main/java/com/pi/poslovna/service/impl/IzvodXMLWriterServiceImpl.java
+++ PoslovnaBanka/src/main/java/com/pi/poslovna/service/impl/IzvodXMLWriterServiceImpl.java
@@ -21,10 +21,12 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import com.pi.poslovna.model.AnalyticsOfStatement;
 import com.pi.poslovna.model.BankAccount;
 import com.pi.poslovna.model.DailyAccountBalance;
 import com.pi.poslovna.service.DailyAccountBalanceService;
 import com.pi.poslovna.service.IzvodXMLWriterService;
+
 
 @Transactional
 @Service
@@ -47,24 +49,25 @@ public class IzvodXMLWriterServiceImpl implements IzvodXMLWriterService {
 			Element rootElement = doc.createElement("izvod_klijenta");
 			doc.appendChild(rootElement);
 			
-			//BROJ RACUNA I DATUM
+			Element zaglavlje = doc.createElement("zaglavlje_preseka");
+			rootElement.appendChild(zaglavlje);
+			
 			Element broj_racuna = doc.createElement("broj_racuna");
 			broj_racuna.appendChild(doc.createTextNode(bankAccount.getAccountNumber()));
-			rootElement.appendChild(broj_racuna);
+			zaglavlje.appendChild(broj_racuna);
 			
 			Element datum = doc.createElement("datum");	
 			datum.appendChild(doc.createTextNode(danas));
-			rootElement.appendChild(datum);
+			zaglavlje.appendChild(datum);
 			
-			//STANJE
 			DailyAccountBalance dnevnoStanje = dnevnoStanjeService.findByTrafficDateAndRacun(sdf.parse(danas), bankAccount);
 			if(dnevnoStanje == null) {
 				Element status = doc.createElement("status");
 				status.appendChild(doc.createTextNode("nema izmena ovog dana"));
-				rootElement.appendChild(status);
+				zaglavlje.appendChild(status);
 			} else {			
 				Element stanje = doc.createElement("stanje");
-				rootElement.appendChild(stanje);
+				zaglavlje.appendChild(stanje);
 			
 				Element prethodno_stanje = doc.createElement("prethodno_stanje");
 				prethodno_stanje.appendChild(doc.createTextNode(Float.toString(dnevnoStanje.getPreviousState())));
@@ -73,6 +76,78 @@ public class IzvodXMLWriterServiceImpl implements IzvodXMLWriterService {
 				Element novo_stanje = doc.createElement("novo_stanje");
 				novo_stanje.appendChild(doc.createTextNode(Float.toString(dnevnoStanje.getNewState())));
 				stanje.appendChild(novo_stanje);
+				
+				for(AnalyticsOfStatement analitika : dnevnoStanje.getMojeAnalitike()) {
+					Element stavka_preseka = doc.createElement("stavka_preseka");
+					
+					Element duznik = doc.createElement("duznik");
+					if(analitika.getDebtor() != null) {
+						duznik.appendChild(doc.createTextNode(analitika.getDebtor()));
+					}
+					stavka_preseka.appendChild(duznik);
+					
+					Element svrha = doc.createElement("svrha_placanja");
+					if(analitika.getPurposeOfPayment() != null) {
+						svrha.appendChild(doc.createTextNode(analitika.getPurposeOfPayment()));
+					}
+					stavka_preseka.appendChild(svrha);
+					
+					Element primalac = doc.createElement("primalac");
+					if(analitika.getRecipient() != null) {
+						primalac.appendChild(doc.createTextNode(analitika.getRecipient()));
+					}
+					stavka_preseka.appendChild(primalac);
+					
+					Element dNalog = doc.createElement("datum_prijema");
+					if(analitika.getDateOfReceipt() != null) {
+						dNalog.appendChild(doc.createTextNode(sdf.format(analitika.getDateOfReceipt())));
+					}
+					stavka_preseka.appendChild(dNalog);
+					
+					Element dValute = doc.createElement("datum_valute");
+					if(analitika.getCurrencyDate() != null) {
+						dValute.appendChild(doc.createTextNode(sdf.format(analitika.getCurrencyDate())));
+					}
+					stavka_preseka.appendChild(dValute);
+					
+					Element racunD = doc.createElement("racun_duznika");
+					if(analitika.getDebtorAccount() != null) {
+						racunD.appendChild(doc.createTextNode(analitika.getDebtorAccount()));
+					}
+					stavka_preseka.appendChild(racunD);
+					
+					Element modelZ = doc.createElement("model_zaduzenja");
+					modelZ.appendChild(doc.createTextNode(Integer.toString(analitika.getModelAssigments())));
+					stavka_preseka.appendChild(modelZ);
+					
+					Element pozivZ = doc.createElement("poziv_na_broj_zaduzenja");
+					if(analitika.getReferenceNumberAssigments() != null) {
+						pozivZ.appendChild(doc.createTextNode(analitika.getReferenceNumberAssigments()));
+					}
+					stavka_preseka.appendChild(pozivZ);
+					
+					Element racunP = doc.createElement("racun_poverioca");
+					if(analitika.getAccountRecipient() != null) {
+						racunP.appendChild(doc.createTextNode(analitika.getAccountRecipient()));
+					}
+					stavka_preseka.appendChild(racunP);
+					
+					Element modelO = doc.createElement("model_odobrenja");
+					modelO.appendChild(doc.createTextNode(Integer.toString(analitika.getModelApproval())));
+					stavka_preseka.appendChild(modelO);
+					
+					Element pozivO = doc.createElement("poziv_na_broj_odobrenja");
+					if(analitika.getReferenceNumberApproval() != null) {
+						pozivO.appendChild(doc.createTextNode(analitika.getReferenceNumberApproval()));
+					}
+					stavka_preseka.appendChild(pozivO);
+					
+					Element iznos = doc.createElement("iznos");
+					iznos.appendChild(doc.createTextNode(Float.toString(analitika.getSum())));
+					stavka_preseka.appendChild(iznos);
+					
+					rootElement.appendChild(stavka_preseka);
+				}
 			}
 			
 			String filePath = path + "izvod.xml";
