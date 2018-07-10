ba3ac9c15e42bbce676165704a95951f66c1ea9e
vuk95 <vuk95novakovic@gmail.com>
2018-07-10 12:13:08 +0200

    Vraceni racuni u data.sql

--- PoslovnaBanka/src/main/resources/data.sql
+++ PoslovnaBanka/src/main/resources/data.sql
@@ -28,13 +28,13 @@ VALUES ("Humska 1",1,"partizan@gmail.com","0113690395","Fudbalski Klub Partizan"
 --RACUNI
 INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,individual_individual_id) values("123456","2018-07-04",true,"1000",2, 3);
 INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,individual_individual_id) values("123457","2018-07-04",true,"100",2, 4);
---INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,legal_entity_legal_id) values("123458","2018-07-09",true,"500",1, 1);
---INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,individual_individual_id) values("123457","2018-07-04",true,"300",1, 5);
+INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,legal_entity_legal_id) values("123458","2018-07-09",true,"500",1, 1);
+INSERT INTO racun(broj_racuna, datum_otvaranja,vazeci,raspoloziva_sredstva,bank_id,individual_individual_id) values("123459","2018-07-04",true,"300",1, 5);
 INSERT INTO moji_racuni_individual (individual_id,racun_id) VALUES(3, 1);
 INSERT INTO moji_racuni_individual (individual_id,racun_id) VALUES(4, 2);
---INSERT INTO moji_racuni_individual (individual_id,racun_id) VALUES(5, 4);
+INSERT INTO moji_racuni_individual (individual_id,racun_id) VALUES(5, 4);
 
---INSERT INTO moji_racuni_legal (legal_id,racun_id) VALUES(1,3);
+INSERT INTO moji_racuni_legal (legal_id,racun_id) VALUES(1,3);
 
 --DRZAVE
 INSERT INTO drzava (oznaka,naziv) values ('SRB','Srbija');
